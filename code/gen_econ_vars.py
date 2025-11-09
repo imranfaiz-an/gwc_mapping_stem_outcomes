e02daaa00b28cdba5b771413073783c1fc265de7
Apollinaire <eabi@uchicago.edu>
2025-11-09 11:37:31 -0600

    cleaned the structure of the project

---
+++ code/gen_econ_vars.py
@@ -0,0 +1,64 @@
+import pandas
+import re
+import yaml
+import typing
+import pathlib
+
+# Load the configuration file
+def load_config(config_path: typing.Optional[pathlib.Path] = None) -> typing.Dict:
+    """Load the configuration file. Default path is the config.yaml file in the code directory."""
+    if config_path is None:
+        config_path = pathlib.Path(__file__).parent / "config.yaml"
+    with open(config_path, 'r') as f:
+        return yaml.safe_load(f)
+
+# Economic characteristics data (unemployment, poverty) from the census data, 2023
+data_econ_source = pandas.read_csv(
+    load_config()["paths"]["data_econ_characteristics"], low_memory=False
+)
+
+# Zip codes of the neighborhoods we are interested in
+ZIP_CODES = [60621, 60636, 60619, 60620, 60623, 60624, 60647, 60651]
+
+data_econ = (
+    # Select the columns we need and rename them
+    # GEO_ID: Unique identifier for the geographic area
+    # NAME: Name of the geographic area
+    # DP03_0005E: Number of unemployed 16 years and older in the civilian workforce
+    # DP03_0003E: Population 16 years and older in the civilian workforce
+    # DP03_0119PE: Percentage of families below the poverty level
+    data_econ_source[['GEO_ID', 'NAME', "DP03_0005E", "DP03_0003E", "DP03_0119PE"]]
+    .rename(columns={
+        "GEO_ID": "geo_id",
+        "NAME": "name",
+        "DP03_0005E": "Number of unemployed 16 years and older in the civilian workforce",
+        "DP03_0003E": "Population 16 years and older in the civilian workforce",
+        "DP03_0119PE": "Percentage of families below the poverty level"
+    })
+    # Convert the columns to numeric
+    .assign(
+        num_unemployed_16_civilian_workforce=lambda x: pandas.to_numeric(
+            x["num_unemployed_16_civilian_workforce"], errors="coerce"
+        ),
+        population_16_civilian_workforce=lambda x: pandas.to_numeric(
+            x["population_16_civilian_workforce"], errors="coerce"
+        ),
+        percent_below_poverty_level=lambda x: pandas.to_numeric(
+            x["percent_below_poverty_level"], errors="coerce"
+        ),
+        unemployment_percentage = lambda x: x["num_unemployed_16_civilian_workforce"] / x["population_16_civilian_workforce"] * 100
+    )
+    # Get the zip code from the geo_id column
+    .assign(
+        zip_code=lambda x: pandas.to_numeric(
+            x["geo_id"].str.split("US").str[1], errors="coerce"
+        ).astype("Int64")
+    )
+    # Drop the geo_id and name columns
+    .drop(columns=["geo_id", "name"])
+    # Reorder the columns to put the zip code first
+    .pipe(lambda df: df[["zip_code"] + [col for col in df.columns if col != "zip_code"]])
+    # Filter to only include the zip codes we need
+    .loc[lambda df: df["zip_code"].isin(ZIP_CODES)]
+    .reset_index(drop=True)
+)
